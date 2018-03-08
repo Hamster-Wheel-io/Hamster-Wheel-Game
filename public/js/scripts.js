f9a76ba9a39a7f2a64a4a193afb2a3f01505e0b5
Briant Oliveira <briantmoliveira@gmail.com>
2018-03-08 15:45:54 -0800

    reverting the website

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -7,7 +7,7 @@
 const mq = window.matchMedia( "(min-width: 600px)" );
 if (mq.matches) {
   // window width is at least 600px
-  console.log($(window).width());
+
 } else {
   // window width is less than 600px
   $('#rainbow').css('display', 'none')
@@ -23,25 +23,14 @@ $('#rainbow3').delay(8000).fadeIn(2000, function() {
     $("#rainbow3").css("z-index", "1");
 });
 
+$(window).on('scroll', () =>{
+    if ( $(window).scrollTop() > 532  && $(window).scrollTop() < 1166){
+        $(".navbar-brand").css('color', 'black');
+    }
+});
+
 // $(window).on('scroll', () =>{
-//     if ( $(window).scrollTop() > 532  && $(window).scrollTop() < 1166){
-//         $(".navbar-brand").css('color', 'black');
-//     }
-// });
-//
-// $(window).on('scroll', () =>{
-//     if (( $(window).scrollTop() > 532 ) && ($(window).scrollTop() < 1166)){
-//         $(".nav-link").css('color', 'black');
-//     }
-//     else {$(".nav-link").css('color', 'white')};
-// });
-//
-// $(window).on('scroll', () =>{
-//     if (( $(window).scrollTop() > 532 ) && ($(window).scrollTop() < 1166)){
-//         $(".nav-link").css('color', 'black');
-//     }
-//     else {$(".navbar-brand").css('color', 'white')};
-// });
+
 
 // $(document).onReady(() => {
 //     if ($(window).width() < 500 )
