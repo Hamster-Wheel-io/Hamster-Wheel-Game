cea293d1c5862bb623e2fb60ca6fbbcbaaf25bbe
Briant Oliveira <briantmoliveira@gmail.com>
2018-04-09 18:04:10 -0700

    finished the img api

--- json/draw.js
+++ json/draw.js
@@ -1,11 +1,9 @@
 
-module.exports = function(app) {
-    [
+module.exports = [
         {
             img: 'http://hostingview.info/wp-content/uploads/2017/12/blank-coloring-pages-blank-coloring-pages-funycoloring-ideas.jpg'
         },
         {
             img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVJXCdqMreTF8S2twnUCZnpoTqK10f4k5K3gI7rEzjYGKka4rHhA'
         }
-    ]
-};
+];
--- routes/images.js
+++ routes/images.js
@@ -2,10 +2,22 @@
 * Images controller
 **************************************/
 require('dotenv').load();
+const draw = require('../json/draw');
+
+
+// // Define JSON File
+//  var fs = require("fs");
+//
+// // Get content from file
+//  var contents = fs.readFileSync("jsoncontent.json");
+// // Define to JSON type
+//  var jsonContent = JSON.parse(contents);
+// // Get Value from JSON
+//
 
 module.exports = function(app) {
 
     app.get('/images', (req, res) => {
-        res.send("imagessss")
-    })
+        res.render('draw', {draw});
+    });
 };
