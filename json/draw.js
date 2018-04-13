a187f4ed39c189cb46e82d61b704e3dcb8dbaba0
Briant Oliveira <briantmoliveira@gmail.com>
2018-04-12 17:28:12 -0700

    added more images on json folder for request

--- json/draw.js
+++ json/draw.js
@@ -5,5 +5,35 @@ module.exports = [
         },
         {
             img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVJXCdqMreTF8S2twnUCZnpoTqK10f4k5K3gI7rEzjYGKka4rHhA'
+        },
+        {
+            img: 'http://www.coloringpages101.com/coloring-pages/Cartoon-Movies/Trolls/Branch-from-Trolls-coloring-page.png'
+        },
+        {
+            img: 'http://cdn2.momjunction.com/wp-content/uploads/2014/09/The-despicableme-a4.jpg'
+        },
+        {
+            img: 'http://www.bilgiuzmani.com/resim/yuklenen/110239-tweety-boyama-8.jpg'
+        },
+        {
+            img: 'http://coloringtop.com/sites/default/files/2_514.jpg'
+        },
+        {
+            img: 'http://www.purse-hanger.com/wp-content/uploads/2018/01/coloring-sheets-for-kids-colouring-pages-for-kids-free-free-coloring-pages-kids-print-naruto-colored-pages.jpg'
+        },
+        {
+            img: 'http://www.purse-hanger.com/wp-content/uploads/2018/01/coloring-sheets-for-kids-best-25-coloring-pages-for-kids-ideas-on-pinterest-kids-bunny-coloring-book.jpg'
+        },
+        {
+            img: 'http://www.purse-hanger.com/wp-content/uploads/2018/01/coloring-sheets-for-kids-best-25-coloring-pages-for-kids-ideas-on-pinterest-kids-baby-pony-coloring-pages.jpg'
+        },
+        {
+            img: 'http://www.purse-hanger.com/wp-content/uploads/2018/01/coloring-sheets-for-kids-best-25-coloring-pages-for-kids-ideas-on-pinterest-kids-coloring-book-drawings.jpg'
+        },
+        {
+            img: 'https://www.coloring-pages-kids.com/coloring-pages/animal-coloring-pages/frogs-coloring-pages/frogs-coloring-pages-images/frog-coloring-page-03.gif'
+        },
+        {
+            img: 'http://www.cavasecreta.com/wp-content/uploads/2018/01/simple-coloring-pages-to-print-simple-coloring-pages-printable-coloring-pages-for-kids-color-in-pictures.jpg'
         }
 ];
